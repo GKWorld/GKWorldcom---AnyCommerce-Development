9c10c0e709907c956f53bd61585e3257022a66d6
Michael Chance <michaelc@zoovy.com>
2013-03-05 15:24:56 -0800

    Revert "No Result Query background"
    This reverts commit 9cae8b4cb6d42ad272e3ca3808128e6c3e59acef.

--- extensions/store_search.js
+++ extensions/store_search.js
@@ -122,11 +122,9 @@ P.query = { 'and':{ 'filters':[ {'term':{'profile':'E31'}},{'term':{'tags':'IS_S
 					$list.parent().find('.resultsHeader').empty().remove(); //remove any previous results multipage headers
 	
 					if(L == 0)	{
-						$list.after("<h2 id='zeroresults' class='contentContainer'>Your query returned zero results.</h2>");
-						
+						$list.append("Your query returned zero results.");
 						}
 					else	{
-						$('#zeroresults').remove();
 						$list.append(app.ext.store_search.u.getElasticResultsAsJQObject(_rtag)); //prioritize w/ getting product in front of buyer
 						if(app.ext.admin)	{
 							$list.hideLoading();
